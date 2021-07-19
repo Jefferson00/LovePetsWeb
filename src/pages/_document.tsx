8bcae71b74973a6608a9ea94c4e8d1fc7a4421e2
Jefferson00 <jeffersonts00@gmail.com>
2021-07-19 16:45:59 -0300

    allow http conection with external api (for demo and tests)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,18 @@
-import Document, {Html, Head, Main, NextScript} from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document{
-    render(){
-        return(
+export default class MyDocument extends Document {
+    render() {
+        return (
             <Html>
                 <Head>
-                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
-                    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
+                    <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
                 </Head>
 
                 <body>
-                    <Main/>
-                    <NextScript/>
+                    <Main />
+                    <NextScript />
                 </body>
             </Html>
         )
